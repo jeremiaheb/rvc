dc9bdcdcacb575a93b782faaad48272fbe890ebb
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-15 12:47:48 -0400

    Initial Commit

---
+++ R/rvc.R
@@ -0,0 +1,24 @@
+#' @title A statistical package for South Florida's Reef Visual Census
+#' @description Provides fisheries managers and scientists with
+#'  summary statistics for South Florida's Reef Visual Census
+#'  using standard statistical procedures
+#' @author \href{mailto:harry.ganz@@noaa.gov}{Harry Ganz}
+#' @docType package
+#' @name rvc
+#' @references
+#' Smith Steven G., Jerald S. Ault, James A. Bohnsack, Douglas E. Harper,
+#' Jiangang Luo, David B. McClellan (2011). "Multispecies survey design
+#' for assesing reef-fish stocks, spatially explicit management performance,
+#' and ecosystem condition". Fisheries Research 109:25-41
+#' \url{http://www.sciencedirect.com/science/article/pii/S0165783611000312}
+#'
+#' Brandt, M.E., N. Zurcher, A. Acosta, J.S. Ault, J.A. Bohnsack, M.W. Feeley, D.E. Harper, J.H. Hunt, T. Kellison, D.B. McClellan, M.E. Patterson, and S.G. Smith (2009).
+#' "A cooperative multi-agency reef fish monitoring protocol for the Florida Keys reef
+#' ecosystem". Natural Resources Report NPS/SFCN/NRR -- 2009/150. National Park Service, Fort Collins, Colorado.
+#' \url{http://www.coris.noaa.gov/activities/fish_monitoring_protocol/}
+#'
+#' Bohnsack, James A., Scott P. Bannerot (1986). "A stationary
+#' visual census technique for qualitatively assesing community
+#' structure of coral reef fishes". NOAA Technical Report NMFS 41.
+#' \url{http://docs.lib.noaa.gov/noaa_documents/NMFS/TR_NMFS/TR_NMFS_41.pdf}
+NULL
