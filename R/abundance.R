fea778fbcd6dfe0403f75ccb6de95008d5590352
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-19 13:56:36 -0400

    Added domain_abundance function

--- R/abundance.R
+++ R/abundance.R
@@ -3,7 +3,7 @@
 
 #' Stratum abundance
 #' @export
-#' @description Number of individuals per stratum based on count. \strong{Note:} Does
+#' @description Number of individuals in a stratum based on count. \strong{Note:} Does
 #' not take into account detection probability.
 #' @inheritParams strat_density
 #' @return A data.frame with abundance per stratum by species, its variance (var),
@@ -19,4 +19,24 @@ strat_abundance = function(x, ntot) {
   names(sdens)[names(sdens) == "density"] = "abundance"
 
   return(sdens)
+}
+
+#' Domain Abundance
+#' @export
+#' @description Number of individuals in a sampling domain based on count. \strong{Note:} Does
+#' not take into account detection probability.
+#' @inheritParams domain_density
+#' @return A data.frame with abundance per sampling domain by species, its variance (var),
+#' the number of SSUs per stratum (nm), the number of PSUs per stratum (n), the total
+#' possible number of SSUs (NM), and the total possible number of PSUs (N)
+domain_abundance = function(x, ntot) {
+  ## Calculate domain density
+  dabun = domain_density(x, ntot)
+  ## Convert density to abundance
+  dabun$density = with(dabun, density*N)
+  dabun$var = with(dabun, var*N^2)
+  ## Rename desnity and return
+  names(dabun)[names(dabun) == "density"] = "abundance"
+
+  return(dabun)
 }
--- tests/testthat/test_abundance.R
+++ tests/testthat/test_abundance.R
@@ -17,3 +17,16 @@ test_that("returns correct variance",
           expect_equal(round(sabun$var), 17614)
           )
 
+context("DOMAIN abundance function")
+
+domain = subset(fk2012, SPECIES_CD == "ANI VIRG")
+
+sdens = strat_density(psu_density(ssu_density(domain)), ntot2012)
+dabun = domain_abundance(sdens, ntot2012)
+
+test_that("returns correct abundance",
+          expect_equal(round(dabun$abundance), 11990)
+          )
+test_that("returns correct variance",
+          expect_equal(round(dabun$var), 1682082)
+          )
