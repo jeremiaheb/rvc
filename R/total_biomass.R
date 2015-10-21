bc4b5cfdfc9497c812a3b06c7ed9b6330d71c07e
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-21 11:29:10 -0400

    Add domain total biomass function

---
+++ R/total_biomass.R
@@ -0,0 +1,47 @@
+#' Total stratum level biomass
+#' @export
+#' @description
+#' Total biomass on the stratum level. Estimated from count
+#' data and allometric growth coefficients. \strong{Note:}
+#' Does not take into account variance in growth parameters
+#' or detection probability
+#' @inheritParams strat_biomass
+#' @returns A data.frame including total stratum biomass (total_biomass),
+#' the average variance in total_biomass (var), number of PSUs sampled (n),
+#' number of SSUs sampled (nm), number of possible PSUs (N), number of
+#' possible SSUs (NM)
+strat_total_biomass = function(x, ntot) {
+  ## Stratum biomass per SSU
+  sbiom = strat_biomass(x, ntot)
+  ## Multiply by number of SSUs
+  sbiom$biomass = with(sbiom, biomass*NM)
+  sbiom$var = with(sbiom, var*NM^2)
+  ## Rename biomass to total_biomass
+  names(sbiom)[names(sbiom) == "biomass"] = "total_biomass"
+
+  return(sbiom)
+}
+
+#' Total sampling domain level biomass
+#' @export
+#' @description
+#' Total biomass on the domain level. Estimated from count
+#' data and allomeric growth coefficients. \strong{Note:}
+#' Does not take into account variance in growth parameters
+#' or detection probability.
+#' @inheritParams domain_biomass
+#' @return A data.frame including total sampling domain biomass (total_biomass)
+#' the average variance in total_biomass (var), number of PSUs sampled (n),
+#' number of SSUs sampled (nm), number of possible PSUs (N), number of
+#' possible SSUs (NM)
+domain_total_biomass = function(x, ntot) {
+  ## Domain biomass per SSU
+  dbiom = domain_biomass(x, ntot)
+  ## Multiply by number of SSUs
+  dbiom$biomass = with(dbiom, NM*biomass)
+  dbiom$var = with(dbiom, NM^2*var)
+  ## Rename biomass to total_biomass
+  names(dbiom)[names(dbiom) == "biomass"] = "total_biomass"
+
+  return(dbiom)
+}
---
+++ tests/testthat/test_total_biomass.R
@@ -0,0 +1,26 @@
+context("STRAT total_biomass function")
+
+load("./../test_data.Rdata")
+
+strat = subset(fk2012, STRAT == "FDLR" & PROT == 0 & SPECIES_CD == "MYC BONA")
+sbiom = strat_total_biomass(psu_biomass(ssu_biomass(strat, list(a = 2e-4, b = 3))), ntot2012)
+
+test_that("returns correct total biomass",
+          expect_equal(round(sbiom$total_biomass), 2274492)
+          )
+test_that("returns correct variance",
+          expect_equal(round(sbiom$var), 438026523625)
+          )
+
+context("DOMAIN total_biomass function")
+
+domain = subset(fk2012, SPECIES_CD == "CEP FULV")
+dbiom = domain_total_biomass(strat_biomass(psu_biomass(ssu_biomass(domain, list(a = 2e-4, b = 2.8))),
+                                           ntot2012), ntot2012)
+
+test_that("returns correct total biomass",
+          expect_equal(round(dbiom$total_biomass), 178186)
+          )
+test_that("returns correct variance",
+          expect_equal(round(dbiom$var), 2746979848)
+          )
