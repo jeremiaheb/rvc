26d6dd76da9eee472ad95620e74bf2668cd0507b
Harry Ganz <hganz@rsmas.miami.edu>
2015-11-09 10:08:48 -0500

    Refactors getSampleData to use RCurl and provide useful messages

--- R/api.R
+++ R/api.R
@@ -13,36 +13,48 @@
 #' A string containing the domain url at which to access the server
 getSampleData = function(years, regions, server = "http://localhost:3000") {
   ## Test that server can be accessed
-  RCurl::url.exists(server)
-  ## Empty list to hold daata
-  l = list()
-  ## Escape spaces
-  regions_esc = URLencode(regions)
-
-  message("downloading files ...")
-  for(yr in seq_along(years)){
-    for(rg in seq_along(regions_esc)){
-      message(paste("downloading sample data for year:", years[yr],"and region:", regions[rg]))
-      ## Set up uri to retrieve file
-      uri = paste(server, "/samples/index.zip?","year=",years[yr],"&","region=",regions_esc[rg], sep="")
-      temp = tempfile() # temp file to store zip
-      ## Download file
-      suppressWarnings(download.file(uri, temp, quiet = TRUE, mode = "wb"))
-      ## If successfully downloaded, unzip and store as data.frame, else warn
-      if(file.size(temp) > 0){
-        message("downloaded. unzipping ...")
-        l[[length(l)+1]] = read.csv(unzip(temp, exdir = tempdir()))
-        unlink(temp) #clean up temporary file
-        message(paste("finished processing sample data for year:", years[yr],"and region:", regions[rg]))
-      } else {
-        msg = paste("failed to retrieve data for year:", years[yr],"and region:", regions[rg])
-        message(msg)
-        warning(msg)
-      }
-    }
-  }
-  ## Rbind data and return
-  sample_data = do.call(rbind, l)
+  if(!RCurl::url.exists(server))stop("could not access server")
+  ## Create url for each query, escape spaces and other URI unsafe characters
+  combined = cbind(years = years, regions = rep(regions, each = length(years)))
+  queries = unlist(lapply(paste(server, '/samples/index.zip?', 'year=', combined[,1], '&region=', combined[,2], sep=""), URLencode))
+  ## Figure out which queries are valid
+  keep = unlist(lapply(queries, RCurl::url.exists))
+  valid_queries = queries[keep]
+  ## If no queries are valid, return error
+  if(length(valid_queries) == 0)stop("could not access data")
+  ## Download data and store into a list
+  message("downloading data ...")
+  data = lapply(valid_queries, download_csv, zipped = TRUE)
   message("done")
-  return(sample_data)
+  if(sum(keep) != nrow(combined)){
+    msg = paste("The following combinations of region/year could not be found:",
+                paste(combined[!keep,2], "/", combined[!keep,1], collapse = ", ", sep = ""))
+    message(msg)
+  }
+  ## Return row bound data
+  return(do.call(rbind, data))
+}
+
+#' Download, unzip, and write a file to a csv
+#' @param u
+#' A string. The URL at which to find the file
+#' @param zipped
+#' A boolean indiating whether a file is zipped file
+#' or not
+download_csv = function(u, zipped) {
+  message('.')
+  ## Read data to a temporary file
+  temp = tempfile()
+  download.file(u, temp, quiet = TRUE)
+  ## Check that file has content
+  if(file.size(temp)<1){stop("there was an error downloading the data")}
+  ## If zipped, unzip and read; otherwise, read directly as csv
+  if(zipped) {
+    out = read.csv(unzip(temp, exdir = tempdir()))
+  } else {
+    out = read.csv(temp)
+  }
+  message('.')
+
+  return(out)
 }
