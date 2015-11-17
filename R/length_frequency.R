f7bf5d590174be4e5d7d890350ede93b81d85437
Harry Ganz <hganz@rsmas.miami.edu>
2015-11-17 13:33:51 -0500

    Removes export and docs for length_frequency lower-level functions

--- R/length_frequency.R
+++ R/length_frequency.R
@@ -1,19 +1,19 @@
 ## Length frequency functions
 
-#' Stratum level lenth frequencies
-#' @export
-#' @description
-#' Calculates length frequency at each stratum
-#' @param x
-#' A data.frame containing columns: YEAR, REGION, STRAT, PROT, PRIMARY_SAMPLE_UNIT,
-#' STATION_NR, NUM, and LEN
-#' @inheritParams strat_density
-#' @return
-#' A data.frame with length frequencies for each stratum
-#' @details
-#' The LEN column should be a numeric column, but the lengths need not
-#' be the original measured lengths. If they are binned, the LEN column
-#' should be transfomed to represent the midpoint of each bin.
+## Stratum level lenth frequencies
+## @export
+## @description
+## Calculates length frequency at each stratum
+## @param x
+## A data.frame containing columns: YEAR, REGION, STRAT, PROT, PRIMARY_SAMPLE_UNIT,
+## STATION_NR, NUM, and LEN
+## @inheritParams strat_density
+## @return
+## A data.frame with length frequencies for each stratum
+## @details
+## The LEN column should be a numeric column, but the lengths need not
+## be the original measured lengths. If they are binned, the LEN column
+## should be transfomed to represent the midpoint of each bin.
 strat_length_frequency = function(x, ntot){
   ## Filter data to include only where individuals are present
   x = count_filter(x, when_present = TRUE)
@@ -37,17 +37,17 @@ strat_length_frequency = function(x, ntot){
   return(out)
 }
 
-#' Domain level length frequency
-#' @export
-#' @description
-#' Calculates the length frequecy at each sampling domain
-#' @param x
-#' A data.frame which is the output of strat_length_frequency
-#' @inheritParams strat_density
-#' @return
-#' A data.frame with: length frequencies for each stratum, the number of PSUs
-#' per domain (n), the number of SSUs per domain (nm), the number of
-#' possible PSUs per domain (N), and the number of possible SSUs per domain (NM)
+## Domain level length frequency
+## @export
+## @description
+## Calculates the length frequecy at each sampling domain
+## @param x
+## A data.frame which is the output of strat_length_frequency
+## @inheritParams strat_density
+## @return
+## A data.frame with: length frequencies for each stratum, the number of PSUs
+## per domain (n), the number of SSUs per domain (nm), the number of
+## possible PSUs per domain (N), and the number of possible SSUs per domain (NM)
 domain_length_frequency = function(x, ntot){
   ## Filter out strata in ntot that are not present in x
   ntot = .with_strata(ntot, x)
